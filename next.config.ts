26b04259634b5c574371fffcfacd42736ed181fd
Vimbisai Basvi <137222478+VimBasvi@users.noreply.github.com>
2026-09-12 18:55:22 -0400

    hero portrait modifications

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { formats: ["image/avif", "image/webp"] },
-  allowedDevOrigins: ["10.65.2.11"],
+  allowedDevOrigins: ["10.65.2.11", '192.168.1.18'],
 };
 
 export default nextConfig;
